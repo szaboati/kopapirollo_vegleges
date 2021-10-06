79ac5f81019a85ff94a0c23490cb7503cb20ed5e
szaboati <47566656+szaboati@users.noreply.github.com>
2021-10-06 10:48:11 +0200

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,37 @@
+input.onGesture(Gesture.Shake, function () {
+    szam = randint(1, 3)
+    if (szam == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(200)
+    } else {
+        if (szam == 1) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . #
+                `)
+            basic.pause(200)
+        } else {
+            basic.showLeds(`
+                # # . . #
+                # # . # .
+                . . # . .
+                # # . # .
+                # # . . #
+                `)
+            basic.pause(200)
+        }
+    }
+})
+let szam = 0
+basic.showIcon(IconNames.Heart)
+basic.pause(1000)
+basic.clearScreen()
